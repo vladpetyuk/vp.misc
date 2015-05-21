273a9ac382b97272d44d68483c149bdc075118c6
vladpetyuk <petyuk@gmail.com>
2015-05-20 19:36:40 -0700

    intro normalization by reference feature

---
+++ R/norm_by_ref.R
@@ -0,0 +1,28 @@
+
+
+
+#' Normalization of SRM Data
+#' 
+#' Subtracts expression data of one condition from another. 
+#' Converts one MSnSet to another MSnSet.
+#' 
+#' @param m MSnSet object
+#' @param exprsn expression with proteins as terms.  
+#'          It is evaluated and used as reference.
+#'          
+#' @importFrom Biobase exprs
+#' @export normalizeByRef
+
+normalizeByRef <- function(m, exprsn){
+    #
+    ref.call <- substitute(exprsn)
+    ref <- eval(ref.call, envir = data.frame(t(exprs(m))))
+    exprs(m) <- sweep(exprs(m), 2, ref, '-') # must be log scale 
+    return(m)
+    
+}
+
+# normalizeByRef(m, MBP.1)
+# normalizeByRef(m, TH.2)
+# normalizeByRef(m, (MBP.1+TH.2)/2)
+
