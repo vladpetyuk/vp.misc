68e09ba2c9018f70ced1b01f32c1439cc29dd4cc
Vlad Petyuk <petyuk@gmail.com>
2015-07-16 21:46:53 -0700

    more options and an example

--- R/norm_by_global.R
+++ R/norm_by_global.R
@@ -1,31 +1,34 @@
 
-
-
-#' Normalization of SRM Data.
+#' Basic Normalization Routines
 #' 
 #' Normalizes data based on assumption that on average peptide deviation 
 #' is zero.  The approach is typically used for global shotgun proteomics
-#' data. Converts one MSnSet to another MSnSet. Uses median polish algorithm.
+#' data. Converts one MSnSet to another MSnSet.
 #' 
-#' @param m MSnSet object
+#' @param m ExpresionSet or MSnSet object
+#' @param method character. "medpolish" - uses median polish algorithm.
+#'          "once" means just correct for median differences in the samples.
+#'          Note, doing it just "once" may de-center the features (rows).
 #' 
 #' @export normalizeByGlob
+#' @examples
+#' data(srm_msnset)
+#' image_msnset(msnset)
+#' msnset2 <- normalizeByGlob(msnset)
+#' image_msnset(msnset2)
 
-normalizeByGlob <- function(m){
+normalizeByGlob <- function(m, method=c("medpolish","once")){
     #
-    # sample.bias <- apply(exprs(m), 2, median, na.rm=T)
-    # exprs(m) <- sweep(exprs(m), 2, sample.bias, '-')
-    out <- medpolish(exprs(m), eps = .Machine$double.eps, 
-                     maxiter = 100, na.rm = T, trace.iter=F)
-    exprs(m) <- out$residuals
+    method <- match.arg(method)
+    #
+    if(method == "once"){
+        mc <- m[complete.cases(exprs(m)),]
+        sample.bias <- apply(exprs(mc), 2, median, na.rm=T)
+        exprs(m) <- sweep(exprs(m), 2, sample.bias, '-')
+    }else if(method == "medpolish"){
+        out <- medpolish(exprs(m), eps = .Machine$double.eps, 
+                         maxiter = 100, na.rm = T, trace.iter=F)
+        exprs(m) <- out$residuals
+    }
     return(m)
-    
 }
-
-# normalizeByRef(m, MBP.1)
-# normalizeByRef(m, TH.2)
-# normalizeByRef(m, (MBP.1+TH.2)/2)
-
-# x <- medpolish(exprs(m), eps = .Machine$double.eps, maxiter = 100, na.rm = F)
-# plot(apply(x$residuals, 2, median, na.rm=T))
-# plot(apply(x$residuals, 1, median, na.rm=T))
