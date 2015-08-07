eaadcb21b23128148348d618ca4e44c7c360a076
vladpetyuk <petyuk@gmail.com>
2015-08-07 16:29:57 -0700

    expanded capability for MaxQuant reader

--- R/readMaxQuantProtGroups.R
+++ R/readMaxQuantProtGroups.R
@@ -1,67 +1,98 @@
 
 
 #' Reading MaxQuant Output
-#' 
-#' Reads "proteinGroups.txt" output as MSnSet. 
-#' 
+#'
+#' Reads in "proteinGroups.txt" output (or its compressed version) as MSnSet.
+#'
 #' So far uses only LFQ data as quantitative data. As feature data, only first
 #' 12 columns and iBAQ columns are used. Features are based on the genes.
-#' In case of multiple proteins matching the same gene, only the one with 
+#' In case of multiple proteins matching the same gene, only the one with
 #' the highest iBAQ value is returned.
 #' @note The "proteinGroups.txt" file can be compressed to save space.
 #' @param path character path to the folder containing "proteinGroups.txt file".
 #'          It should be in "{raw files folder}/combined/txt"
+#' @param quantType character Defines pattern what type of column to
+#'                  use for quantificaiton. 
+#'                  E.g. "LFQ intensity" or "Ratio H/L normalized".
+#' @param verbose numeric controls the text output
+#' 
+#' @note It looks like the convention for column naming in MaxQuant is
+#'       type of quantification followed by sample name separated by space.
+#'       E.g. "LFQ intensity Sample1" or "Ratio H/L normalized Control33".
+#'       In \code{quantType} you need to specify the full first component
+#'       in the name that defines the type of quantification.
+#'       
+#' @note iBAQ option must be enabled in MaxQuant analysis because it is used
+#'       to resolve ambiguity between two proteins matching one gene.
+#'
 #' @return \code{MSnSet} object
-#' @importFrom Biobase lcPrefix
 #' @importFrom MSnbase MSnSet
 #' @importFrom plyr ddply
 #' @export readMaxQuantProtGroups
 #' @examples
+#' 
+#' # label-free data
 #' m <- readMaxQuantProtGroups(system.file("extdata/MaxQuant",
-#'                                          package="vp.misc"))
-#' library("vp.misc")
+#'                                          package="vp.misc"),
+#'                             quantType="LFQ intensity")
 #' exprs(m) <- log2(exprs(m))
 #' exprs(m) <- sweep(exprs(m), 1, rowMeans(exprs(m), na.rm=TRUE), '-')
 #' image_msnset(m)
 #' 
-readMaxQuantProtGroups <- function(path){
+#' # O18/O16 data
+#' m <- readMaxQuantProtGroups(system.file("extdata/MaxQuant_O18",
+#'                                          package="vp.misc"),
+#'                             quantType="Ratio H/L normalized")
+#' exprs(m) <- log2(exprs(m))
+#' exprs(m) <- sweep(exprs(m), 1, rowMeans(exprs(m), na.rm=TRUE), '-')
+#' image_msnset(m)
+#' 
+#' 
+readMaxQuantProtGroups <- function(path, quantType, verbose=1){
     # no options in the current version
     # use genes for IDs
-    # and LFQ for quant data
-    
+
     # fpath <- file.path(path, "proteinGroups.txt")
     # I assume the file can be compressed for the sake of space
-    # thus ther is a bit relaxed search for file
-    fpath <- 
-        list.files(path = path, 
+    # thus there is a bit relaxed search for file
+    fpath <-
+        list.files(path = path,
                    pattern = "proteinGroups.txt",
                    full.names = TRUE)
     stopifnot(length(fpath) == 1)
-    
+
     x <- read.delim(fpath, check.names = FALSE, stringsAsFactors = FALSE)
-    
+    if(verbose > 0){
+        print("MaxQuant columns")
+        print(colnames(x))
+    }
+
     # feature IDs in first 12 columns
-    id.cols <- c("Protein IDs", 
-                 "Majority protein IDs", 
+    id.cols <- c("Protein IDs",
+                 "Majority protein IDs",
                  "Peptide counts (all)",
-                 "Peptide counts (razor+unique)", 
-                 "Peptide counts (unique)", 
-                 "Protein names", 
-                 "Gene names", 
-                 "Fasta headers", 
-                 "Number of proteins", 
-                 "Peptides", 
+                 "Peptide counts (razor+unique)",
+                 "Peptide counts (unique)",
+                 "Protein names",
+                 "Gene names",
+                 "Fasta headers",
+                 "Number of proteins",
+                 "Peptides",
                  "Razor + unique peptides",
                  "Unique peptides")
-    ibac.col <- "iBAQ" # to resolve gene ambiguity
-    quant.cols <- grep("^LFQ", colnames(x), value = TRUE)
+    ibac.col <- "iBAQ" # to resolve gene ambiguity in situations when
+                       # we need to select one gene per protein id.
+                       # that ".+" is pretty much a hack to ensure that 
+                       # I do not read in columns that have nothing to do 
+                       # with samples
+    quant.cols <- grep(paste("^",quantType,".+",sep=''), colnames(x), value = TRUE)
     x <- x[,c(id.cols, ibac.col, quant.cols)]
-    
-    # trim the quant.cols name
-    pref <- Biobase::lcPrefix(quant.cols)
+
+    # trim the quant.cols name and retain only sample names
+    pref <- paste(quantType,"\\s+",sep="")
     colnames(x) <- sub(pref, '', colnames(x))
     quant.cols <- sub(pref, '', quant.cols)
-    
+
     #
     #.. GENE LEVEL
     # Let's get rid of "CON" "REV" and empty gene names, then check for redundancy.
@@ -74,15 +105,16 @@ readMaxQuantProtGroups <- function(path){
     # retain genes with higher iBAQ
     x <- plyr::ddply(.data = x, .variables = ~ feature.name,
                      .fun = function(d){d[which.max(d$iBAQ),]})
-    
+
     #.. get dataset names
-    fpath <- 
-        list.files(path = path, 
+    # dataset names are in the summary.txt
+    fpath <-
+        list.files(path = path,
                    pattern = "summary.txt",
                    full.names = TRUE)
     stopifnot(length(fpath) == 1)
     smmr <- read.delim(fpath, check.names = FALSE, stringsAsFactors = FALSE)
-    warning("Asumming \"summary.txt\" file contains 2n+1 rows. 
+    warning("Asumming \"summary.txt\" file contains 2n+1 rows.
 Where n is the number of datasets.
 Please double check.")
     smmr <- smmr[seq_len((nrow(smmr)-1)/2),]
@@ -94,16 +126,16 @@ Please double check.")
     x.exprs <- as.matrix(x[,quant.cols])
     x.exprs[x.exprs == 0] <- NA
     rownames(x.exprs) <- x$feature.name
-    x.pdata <- data.frame(sample.name = colnames(x.exprs), 
+    x.pdata <- data.frame(sample.name = colnames(x.exprs),
                           dataset.name = smmr[colnames(x.exprs),],
                           stringsAsFactors = FALSE)
     rownames(x.pdata) <- colnames(x.exprs)
     x.fdata <- x[,c(id.cols, ibac.col)]
     rownames(x.fdata) <- x$feature.name
-    ans <- MSnbase::MSnSet(exprs = x.exprs, 
+    ans <- MSnbase::MSnSet(exprs = x.exprs,
                            fData = x.fdata,
                            pData = x.pdata)
-    if (validObject(ans)) 
+    if (validObject(ans))
         return(ans)
 }
 
