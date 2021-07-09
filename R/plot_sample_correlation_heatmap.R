cb3f7281cca4b153522b5891161709efbeb8a66f
Tyler Sagendorf <57922480+TylerSagendorf@users.noreply.github.com>
2021-07-08 17:24:33 -0700

    Fix documentation

--- R/plot_sample_correlation_heatmap.R
+++ R/plot_sample_correlation_heatmap.R
@@ -3,49 +3,53 @@
 #' Wrapper functions to generate correlation matrix plots
 #' for MSnSet objects. There is a parameter for the phenotype to
 #' order the columns (samples) by.
-#' 
-#' @param msnset (MSnSet)
+#'
+#' @param m an MSnSet object
 #' @param phenotype (character) name of the column to order by (usually plex)
-#' 
-#' @param ... additional params to the heatmap call
+#'
+#' @param ... additional arguments passed to \code{\link[stats]{heatmap}}
 #'
 #' @return (list) plot object
 #'
-#' @importFrom MSnbase pData exprs 
-#' @importFrom dplyr %>% select 
-#' @importFrom pheatmap pheatmap
+#' @export plot_sample_correlation_heatmap
+#'
+#' @importFrom MSnbase pData exprs
+#' @importFrom dplyr %>% select
 #'
 #' @examples
-#' 
+#'
 #' library(MSnSet.utils)
 #' data(cptac_oca)
 #'
 #' m <- oca.set
-#' 
+#'
 #' # Using base heatmap
 #' plot_sample_correlation_heatmap(m, "Batch")
-#' 
+#'
 #' # Using `pheatmap` (pretty heatmap)
 #' plot_sample_correlation_pheatmap(m, "Batch")
-#' 
 
-#' @export
+
 plot_sample_correlation_heatmap <- function(m, phenotype, ...) {
     m <- m[,order(pData(m)[,phenotype])]
     x <- cor(exprs(m), use = "complete.obs")
     x <- x[nrow(x):1, ]
-    heatmap(x, Rowv = NA, Colv = NA, 
+    heatmap(x, Rowv = NA, Colv = NA,
             scale = "none", symm = TRUE,
             ...)
 }
 
-#' @export
-plot_sample_correlation_pheatmap <- function(m, phenotype, ...) { 
+
+
+#' @describeIn plot_sample_correlation_heatmap Using the \code{pheatmap} package
+#' @importFrom pheatmap pheatmap
+#' @export plot_sample_correlation_pheatmap
+plot_sample_correlation_pheatmap <- function(m, phenotype, ...) {
     m <- m[,order(pData(m)[,phenotype])]
     x <- cor(exprs(m), use = "complete.obs")
     pheno = pData(m) %>%
         select(!!phenotype)
-    pheatmap(x, 
+    pheatmap(x,
              cluster_rows = FALSE, cluster_cols = FALSE,
              annotation_col = pheno, annotation_row = pheno,
              ...)
