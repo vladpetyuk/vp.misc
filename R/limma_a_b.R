9f7824b6bbf8c5b9e1ed694f957a78b97c297f5b
Vlad Petyuk <petyuk@gmail.com>
2022-06-22 16:01:33 -0700

    adding standard error capture to limma_gen

--- R/limma_a_b.R
+++ R/limma_a_b.R
@@ -118,6 +118,8 @@ limma_gen <- function(eset, model.str, coef.str, ...){
     fit.smooth <- eBayes(fit)
     sig <- topTable(fit.smooth, number=nrow(eset),
                     sort.by='none', coef=coef.str)
+    se <- (sqrt(fit.smooth$s2.post) * fit.smooth$stdev.unscaled)[,coef.str]
+    sig$SE <- as.numeric(se)
     return(sig)
 }
 
