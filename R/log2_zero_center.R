28a5edfe715483e79f65e5a7f2076e8c1b688a18
skelly001 <97435404+skelly001@users.noreply.github.com>
2023-05-31 12:20:26 -0700

    Create convenience functions for 1) filtering exprs(MSnSet) missingness 2) log2 and center exprs(MSnSet)

---
+++ R/log2_zero_center.R
@@ -0,0 +1,33 @@
+
+#' Log2 transform and center mean around zero
+#'
+#' Log2 transform applied to "exprs" element of ExpressionSet or MSnSet object
+#' followed by centering the arithmetic mean around zero. Converts one
+#' MSnSet to another MSnSet.
+#'
+#' @param m ExpresionSet or MSnSet object
+#'
+#' @importFrom Biobase exprs<-
+#'
+#' @export log2_zero_center
+#'
+#' @examples
+#' data(srm_msnset)
+#' msnset2 <- log2_zero_center(msnset)
+
+
+log2_zero_center <- function(m){
+
+  exprs(m) <- log2(exprs(m))
+
+  if (any(is.infinite(exprs(m))) == TRUE) {
+    warning("After transformation, infinite values are present.")
+  }
+
+  exprs(m) <- sweep(exprs(m),
+                    MARGIN = 1,
+                    STATS = apply(exprs(m), 1, mean, na.rm = TRUE),
+                    FUN = "-")
+
+  return(m)
+}
