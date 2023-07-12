6817bc9f92897125a51593db816e8f799e64d44f
TylerSagendorf <tjsagendorf@gmail.com>
2023-07-12 09:42:59 -0700

    Document deprecated functions

---
+++ R/deprecated-functions.R
@@ -0,0 +1,12 @@
+#' @title Deprecated functions in package \pkg{MSnSet.utils}.
+#'
+#' @description The functions listed below are deprecated and will be defunct in
+#'   the near future. When possible, alternative functions with similar
+#'   functionality are also mentioned. Help pages for deprecated functions are
+#'   available at \code{help("<function>-deprecated")}.
+#'
+#' @name MSnSet.utils-deprecated
+#'
+#' @keywords internal
+NULL
+
